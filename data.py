2a7789e88987e0cc42d43ef5c5e0d0b55da08537
franco-swarts-up <u21823091@tuks.co.za>
2021-11-14 14:30:31 +0200

    testing new training set

--- data.py
+++ data.py
@@ -80,6 +80,62 @@ def transpose(x, y, width, height, dir):
 
     return rx, ry
 
+map = {}
+
+for line in open('./data/train/train.csv'):
+    if line.startswith('Image_ID'):
+        continue
+    data = line.split(',')
+    image_id = data[0]
+    label = data[1]
+
+    if not map.__contains__(image_id):
+        map[image_id] = [0, 0, 0]
+
+    map[image_id][class_map[label]] += 1
+
+
+counts = [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]
+for i in map:
+    index = sum(map[i])
+    for j in range(len(map[i])):
+        counts[index][j] += map[i][j]
+
+
+maximum = 10000
+for i in counts:
+    for j in i:
+        if j != 0 and j < maximum:
+            maximum = j
+
+usages = [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]
+
+training_set = []
+
+for id in map:
+
+    count = sum(map[id])
+
+    if count > 4:
+        continue
+
+    full = False
+
+    for i in range(len(usages[count])):
+        if map[id][i] + usages[count][i] > maximum:
+            full = True
+
+    if full:
+        continue
+
+    for i in range(len(map[id])):
+        usages[count][i] += map[id][i]
+
+    training_set.append(id)
+
+print(usages)
+print(training_set)
+exit(0)
 
 count = 0
 
@@ -110,6 +166,9 @@ for line in open('./data/train/train.csv'):
                                              height / 512))
     label_file.flush()
     label_file.close()
+
+    if image_id not in training_set:
+        continue
 
     for i in [0, 90, 180, 270]:
         rotated_image = image.rotate(i)
--- tst.py
+++ tst.py
@@ -1,9 +1,65 @@
 from os import listdir
 
-count = 0
+map = {}
 
-for f in listdir('./data/labels/training'):
-    for line in open('./data/labels/training/{}'.format(f)):
-        count += 1
+class_map = {
+    'fruit_woodiness': 0,
+    'fruit_brownspot': 1,
+    'fruit_healthy': 2
+}
 
-print(count)
+for line in open('./data/train/train.csv'):
+    if line.startswith('Image_ID'):
+        continue
+    data = line.split(',')
+    image_id = data[0]
+    label = data[1]
+
+    if not map.__contains__(image_id):
+        map[image_id] = [0, 0, 0]
+
+    map[image_id][class_map[label]] += 1
+
+
+counts = [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]
+for i in map:
+    index = sum(map[i])
+    for j in range(len(map[i])):
+        counts[index][j] += map[i][j]
+
+
+max = 10000
+for i in counts:
+    for j in i:
+        if j != 0 and j < max:
+            max = j
+
+usages = [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]
+
+training_set = []
+
+for id in map:
+
+    count = sum(map[id])
+
+    if count > 4:
+        continue
+
+    full = False
+
+    for i in range(len(usages[count])):
+        if map[id][i] + usages[count][i] > max:
+            full = True
+
+    if full:
+        continue
+
+    for i in range(len(map[id])):
+        usages[count][i] += map[id][i]
+
+    training_set.append(id)
+
+print(training_set)
+
+
+
